385dcb542b98bce1c8c8cb63c9d843c4b31dc7ef
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-11-09 09:25:55 -0700

    Add properties to rooms class based off of tests

--- test/rooms-test.js
+++ test/rooms-test.js
@@ -13,6 +13,32 @@ describe('Rooms', () => {
   })
   
   it('should be a function', () => {
-    expect(Customers).to.be.a('function')
+    expect(Rooms).to.be.a('function')
+  })
+  it('should have a room number', () => {
+    expect(room1.number).to.equal(1)
+    expect(room2.number).to.equal(2)
+  })
+  it('should have a room type', () => {
+    expect(room1.roomType).to.equal('residential suite')
+    expect(room2.roomType).to.equal('suite')
+  })
+  it('should have a bidet', () => {
+    expect(room1.bidet).to.equal(true)
+  })
+  it('should not have a bidet', () => {
+    expect(room2.bidet).to.equal(false)
+  })
+  it('should have a bedsize', () => {
+    expect(room1.bedSize).to.equal('queen')
+    expect(room2.bedSize).to.equal('full')
+  })
+  it('should have a bed count', () => {
+    expect(room1.numBeds).to.equal(1)
+    expect(room2.numBeds).to.equal(2)
+  })
+  it('should have a cost per night', () => {
+    expect(room1.costPerNight).to.equal(358.4)
+    expect(room2.costPerNight).to.equal(477.38)
   })
 })
